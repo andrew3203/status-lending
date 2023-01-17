a548d50e7cd7e7cc8b2ee3f26d03b909b5dd840a
kuand <kuand3203@gmail.com>
2023-01-17 13:37:43 +0300

    add animotion

--- app/client/static/js/interactions.js
+++ app/client/static/js/interactions.js
@@ -3,8 +3,17 @@ $(".close-btn").click(function () {
     $(".modal").hide();
 });
 
-
 $('.open-modal').click(function () {    
     $(".modal").fadeIn(300);
     $(".modal").show();
+});
+
+$('.open-modal2').click(function () {    
+    $(".modal2").fadeIn(300);
+    $(".modal2").show();
+});
+
+$(".close-btn2").click(function () {    
+    $(".modal2").fadeOut(300);
+    $(".modal2").hide();
 });
