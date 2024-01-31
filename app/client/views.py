1579f44045a234c0ff79536b2b595ca4f2869fc1
Andrey <44374839+andrew3203@users.noreply.github.com>
2024-02-01 02:46:43 +0300

    add neew mail

--- app/client/views.py
+++ app/client/views.py
@@ -1,16 +1,17 @@
-from django.views.generic import DetailView
-from django.shortcuts import render
+import logging
+
 from client import serializers
-from rest_framework import generics
-from client.models import Complex, Client, SiteData
-from django.http import HttpResponseNotFound
-from rest_framework.response import Response
-from rest_framework import status
-from django.core.mail import send_mail
+from client.models import Client, Complex, SiteData
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.translation import get_language, activate, gettext
-import logging
+from django.core.mail import send_mail
+from django.http import HttpResponseNotFound
+from django.shortcuts import render
+from django.utils.translation import activate, get_language, gettext
+from django.views.generic import DetailView
+from rest_framework import generics, status
+from rest_framework.response import Response
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -50,18 +51,27 @@ class ClientCreateAPIView(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-       
+
         try:
             serializer.is_valid(raise_exception=True)
             contact = serializer.save()
             message = f"\nНовый контакт {contact.site}\n\nИмя: {contact.name}\nПочта: {contact.email}\nТелефон: {contact.phone}\nЖК: {contact.site}"
+
+            # Список получателей
+            recipient_list = settings.RECIPIENT_ADDRESS
+
+            # Проверка домена
+            if 'hleb-dom.ru' in request.get_host():
+                recipient_list = recipient_list + ['sovetniktv@mail.ru']
+
             send_mail(
-                subject=f'Новый контакт {str(contact.site).upper()}', 
+                subject=f'Новый контакт {str(contact.site).upper()}',
                 message=message,
                 from_email=settings.EMAIL_HOST_USER,
-                recipient_list=settings.RECIPIENT_ADDRESS,
+                recipient_list=recipient_list,
             )
         except Exception as e:
             print(e)
+
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
